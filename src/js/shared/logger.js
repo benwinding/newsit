3908da06050f7b5d0dafa30d40f7bd92b85f0c92
Ben Winding <hello@benwinding.com>
2020-10-20 13:44:28 +1030

    Fix logger

--- src/js/shared/logger.js
+++ src/js/shared/logger.js
@@ -6,24 +6,24 @@ function isProduction() {
   return process.env.IS_PRODUCTION;
 }
 
-function MakeLogger(prefixString) {
-  function prefixString() {
-    return `> newsit: ${prefixString} `;
+function MakeLogger(prefixStr) {
+  function getPrefixString() {
+    return `> newsit: ${prefixStr} `;
   }
   return {
     get log() {
       // if (isProduction()) {
       //   return (...any) => {};
       // }
-      const boundLogFn = console.log.bind(console, prefixString());
+      const boundLogFn = console.log.bind(console, getPrefixString());
       return boundLogFn;
     },
     get error() {
-      const boundLogFn = console.error.bind(console, prefixString());
+      const boundLogFn = console.error.bind(console, getPrefixString());
       return boundLogFn;
     },
     get warn() {
-      const boundLogFn = console.warn.bind(console, prefixString());
+      const boundLogFn = console.warn.bind(console, getPrefixString());
       return boundLogFn;
     },
   }
