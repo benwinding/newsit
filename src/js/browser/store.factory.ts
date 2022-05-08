91a05ccd3217e986a5330f5cff279798c620bbee
Ben Winding <hello@benwinding.com>
2022-05-09 08:57:49 +1000

    TIghten store types

--- src/js/browser/store.factory.ts
+++ src/js/browser/store.factory.ts
@@ -1,7 +1,9 @@
 import { MakeLogger } from "../shared/logger.factory";
 import { getBrowserInstance } from "./browser";
+import { IStore } from "./istore";
 import { Store } from "./store";
 
-export const store = new Store(getBrowserInstance());
-export const logger = MakeLogger("store", store);
-store._SetLogger(logger);
+const _store = new Store(getBrowserInstance());
+export const store: IStore = _store;
+export const logger = MakeLogger("store", _store);
+_store._SetLogger(logger);
